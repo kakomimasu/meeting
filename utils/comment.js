8bd9b50d791832310abd96e263fbc4b2bf689cf4
ninja03 <69106571+ninja03@users.noreply.github.com>
2023-06-17 01:07:22 +0900

    format

--- utils/comment.js
+++ utils/comment.js
@@ -1,29 +1,29 @@
 export class Comment {
-    constructor(user, message) {
-        this.user = user
-        this.message = message
-        this.deleted = false
+  constructor(user, message) {
+    this.user = user;
+    this.message = message;
+    this.deleted = false;
 
-        this.id = new Date().getTime() // UNIX時間のミリ秒
-        this.sentAt = new Date
-    }
-    delete() {
-        this.deleted = true
-    }
+    this.id = new Date().getTime(); // UNIX時間のミリ秒
+    this.sentAt = new Date();
+  }
+  delete() {
+    this.deleted = true;
+  }
 
-    get getId() {
-        return this.id
-    }
-    get getUser() {
-        return this.user
-    }
-    get getMessage() {
-        return this.message
-    }
-    get getSentAt() {
-        return this.sentAt
-    }
-    get isDeleted() {
-        return this.deleted
-    }
+  get getId() {
+    return this.id;
+  }
+  get getUser() {
+    return this.user;
+  }
+  get getMessage() {
+    return this.message;
+  }
+  get getSentAt() {
+    return this.sentAt;
+  }
+  get isDeleted() {
+    return this.deleted;
+  }
 }
