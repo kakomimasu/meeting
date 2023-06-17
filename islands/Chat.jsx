d886c318ea0c1de89b815b55c9c8ca3ba74a5e54
ninja03 <69106571+ninja03@users.noreply.github.com>
2023-06-17 14:48:19 +0900

    chat

--- islands/Chat.jsx
+++ islands/Chat.jsx
@@ -1,14 +1,60 @@
-export default function Chat() {
+import { useEffect, useRef } from "preact/hooks";
+import { useSignal } from "@preact/signals";
+
+export default function Chat({ user }) {
+  const data = useSignal([]);
+  const input = useRef();
+
+  useEffect(async () => {
+    if (!user) {
+      return;
+    }
+
+    data.value = await (await fetch("/chat")).json();
+
+    let es = new EventSource(window.location.href + "chat");
+    es.addEventListener("message", (e) => {
+      data.value = JSON.parse(e.data);
+    });
+
+    es.addEventListener("error", async () => {
+      es.close();
+      const backoff = 10000 + Math.random() * 5000;
+      await new Promise((resolve) => setTimeout(resolve, backoff));
+      es = new EventSource(window.location.href + "chat");
+    });
+  }, []);
+
+  const send = async () => {
+    const form = new FormData();
+    form.append("msg", input.current.value);
+    await fetch("chat", {
+      method: "POST",
+      body: form,
+    });
+    input.current.value = "";
+  };
+
   return (
     <>
-      <script type="module" src="chat.js" />
-      <div class="chat">
-        <div class="message-div">
-          <table id="messages"></table>
+      <div style={{ width: "50%" }}>
+        <div
+          style={{
+            height: "350px",
+            overflow: "scroll",
+          }}
+        >
+          <table>
+            {data.value.reverse().map((d) => (
+              <tr>
+                <td>{d}</td>
+              </tr>
+            ))}
+          </table>
         </div>
         <div>
-          <input id="input-msg" />
-          <button id="send">送信</button>
+          <input ref={input} style={{ display: "inline" }} />
+          <button onClick={send} style={{ display: "inline" }}>送信</button>
         </div>
       </div>
     </>
--- routes/index.jsx
+++ routes/index.jsx
@@ -37,7 +37,7 @@ export default function Page({ data }) {
           <div id="remote-media-area"></div>
         </div>
 
-        <Chat />
+        <Chat user={user} />
       </div>
     </>
   );
