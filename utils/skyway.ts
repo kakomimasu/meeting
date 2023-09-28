c7e69c8037c1bad23d2a55a32fd023599bd2cbfa
SuzuTomo <47011206+kamekyame@users.noreply.github.com>
2023-09-28 12:01:54 +0000

    fix: ROOM_NAMEをexportしていないバグを修正

--- utils/skyway.ts
+++ utils/skyway.ts
@@ -2,7 +2,8 @@ import { SkyWayAuthToken } from "skyway-token";
 
 const APP_ID = Deno.env.get("SKYWAY_ID") as string;
 const SECRET_KEY = Deno.env.get("SKYWAY_SECRET") as string;
-const ROOM_NAME = Deno.env.get("SKYWAY_ROOM") ?? "kakomimasu-meeting-local";
+export const ROOM_NAME = Deno.env.get("SKYWAY_ROOM") ??
+  "kakomimasu-meeting-local";
 
 // export const ROOM_NAME = "kakomimasu-meeting";
 
